dd81b4d78f3263074f8351bf2c31127d27990fb2
Matea <matea.nikolac95@gmail.com>
2024-02-22 09:53:11 +0100

    edit code structure for better readability

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 // https://bun.sh/
-import { file } from 'bun'
-import { appendFile, mkdir } from 'node:fs/promises'
-import fs from 'fs'
+
 import dictionary from './dictionary.json'
 
 const host = 'www.dow.com'
@@ -10,69 +8,50 @@ const server = Bun.serve({
   port: 36107,
   fetch: async (request) => {
     try {
-      const res = await fetch(`https://${host}`)
-      
-      const content = await res.text()
-      const url = new URL(request.url)
     
-      // prevent the extra call for /favicon.ico and ensure the code runs only once
-      if (url.href !== `http://localhost:${url.port}/favicon.ico`){
+      // Remove the 'http://localhost:36107' part from the request URL
+      const url = request.url.replace('http://localhost:36107', '')
 
-        // send the HTML content via Response
-        if (request.url === 'http://localhost:36107/') {
-          let htmlContent = content
+      // Fetch data from the url which is a combination of the host URL and the cleaned URL  
+      const res = await fetch(`https://${host}${url}`)
 
-          //! part 2
-          // use dictionary.json to change any word on the displayed site
+      // Extract the text content from the response
+      let content = await res.text()
 
-          for (const word in dictionary){
-            const replacementWord = dictionary[word]
-            const originalWord = word
-            const regex = new RegExp(originalWord, 'g')
-
-            htmlContent = htmlContent.replace(regex, replacementWord)
-          }
-
-          return new Response(htmlContent, {
-            headers: {
-              'Content-Type' : 'text/html',
-            },
-          })
-        } else {
-
-          // Fetch content from the requested URL
-
-          // Remove the 'http://localhost:36107' part from the request URL
-          const cleanUrl = request.url.replace('http://localhost:36107', '')
-          // Fetch data from the combined host URL and cleaned URL
-          const cleanUrlRes = await fetch(`https://${host}${cleanUrl}`)
-          // Extract the text content from the response of the cleaned URL
-          const cleanUrlContent = await cleanUrlRes.text()
-
-
-          // check the file extention of the cleanUrl to determine the conent type
-          let contentType = ''
-
-          if (cleanUrl.includes('.css')){
-            contentType = 'text/css'
-          } else if (cleanUrl.includes('.json')){
-            contentType = 'application/json;charset=utf-8'
-          } else if (cleanUrl.includes('.js')){
-            contentType = 'application/javascript'
-          } else if  (cleanUrl.includes('.svg')) {
-            contentType = 'image/svg+xml'
-          }
-
-          // Create a new Response object with the content and Content-Type headers
-          return new Response(cleanUrlContent, {
-            headers: {
-              'Content-Type' : `${contentType}`,
-            },
-          })
-        }
+      // use dictionary.json to change any word on the displayed site 
+      for (const word in dictionary){
+        const replacementWord = dictionary[word]
+        const originalWord = word
+        const regex = new RegExp(originalWord, 'g')
+        content = content.replace(regex, replacementWord)
       }
+
+      // check the file extention of the url to determine the conent type
+      let contentType = ''
+
+      if (url.includes('.css')){
+        contentType = 'text/css'
+      } else if (url.includes('.json')){
+        contentType = 'application/json;charset=utf-8'
+      } else if (url.includes('.js')){
+        contentType = 'application/javascript'
+      } else if  (url.includes('.svg')) {
+        contentType = 'image/svg+xml'
+        // check if this either a request for the home page or for any other page that contains .html
+      } else if (url.includes('.html') || url === '/'){
+        contentType = 'text/html'
+      }
+
+      // Create a new Response object with the content and Content-Type headers
+      return new Response(content, {
+        headers: {
+          'Content-Type' : `${contentType}`,
+        },
+      })
+      
+      
     } catch (error) {
-      console.error('Error', error)
+      console.log('Error', error)
       return new Response('Error', { status: 500 })
     }
   },
